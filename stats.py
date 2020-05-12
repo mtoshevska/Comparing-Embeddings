d26a0a8f813084d11cac3ec463bf437ac287faf0
Martina Toshevska <martina.tosevska.95@gmail.com>
2020-05-12 21:54:18 +0200

    Final statistics

--- stats.py
+++ stats.py
@@ -1,18 +1,22 @@
 import pandas as pd
 from math import isnan
 import numpy as np
+from itertools import repeat
 
 
 def average_word_similarity(dataset_name, emb_name, emb_type, emb_size):
     dataset = pd.read_csv(f'results/similarity/{dataset_name}_{emb_name}_{emb_type}_{emb_size}_cosine.csv')
     s1 = dataset['gt_sim'].get_values()
     s2 = dataset['cosine_sim'].get_values()
+    nan_values = 0
     for i in range(len(s2) - 1, 0, -1):
         if isnan(s2[i]):
+            nan_values += 1
             s1 = np.delete(s1, i)
             s2 = np.delete(s2, i)
     print('==================================================')
     print(f'Dataset: {dataset_name}, Embeddings: {emb_name}-{emb_type}-{emb_size}')
+    print(f'NaN: {nan_values}')
     print(f'Average gt similarity: {round(np.mean(s1), 2)}')
     print(f'Average cosine similarity: {round(np.mean(s2), 2)}')
     print('==================================================')
@@ -37,7 +41,7 @@ def average_word_similarity_pos(dataset_name, emb_name, emb_type, emb_size):
         print()
 
 
-if __name__ == '__main__':
+def print_average_word_similarities():
     average_word_similarity('WordSim353', 'glove', 'wikipedia', 50)
     average_word_similarity('WordSim353', 'glove', 'wikipedia', 300)
     average_word_similarity('WordSim353', 'glove', 'twitter', 50)
@@ -125,3 +129,30 @@ if __name__ == '__main__':
     average_word_similarity_pos('SimLex999', 'lexvec', 'crawl', 300)
     average_word_similarity_pos('SimLex999', 'word2vec', 'googlenews', 300)
     average_word_similarity_pos('SimLex999', 'numberbatch', 'conceptnet', 300)
+
+
+def construct_correlation_table(datasets, embeddings):
+    data = pd.DataFrame()
+    embeddings_list = ['-'.join([str(i) for i in e]) for e in embeddings]
+    data['Word Embeddings'] = [x for item in embeddings_list for x in repeat(item, 3)]
+    data['Correlation'] = ['S', 'Pr', 'K'] * len(embeddings_list)
+    for dataset_name in datasets:
+        values = []
+        for emb_name, emb_type, emb_size in embeddings:
+            p, k, s = \
+                pd.read_table(f'results/correlation/{dataset_name}_{emb_name}_{emb_type}_{emb_size}.csv', sep=',')[
+                    'correlation_coefficient'].get_values()
+            values.extend([s, p, k])
+        data[dataset_name] = values
+    data.to_csv('results/correlation_coefficients.csv', index=None)
+
+
+if __name__ == '__main__':
+    print_average_word_similarities()
+
+    construct_correlation_table(['WordSim353', 'SimLex999', 'SimVerb3500', 'RG65', 'RW2034', 'Verb143'],
+                                [('glove', 'wikipedia', 50), ('glove', 'wikipedia', 300),
+                                 ('glove', 'twitter', 50), ('glove', 'twitter', 200),
+                                 ('fasttext', 'wikipedia', 300), ('fasttext', 'crawl', 300),
+                                 ('lexvec', 'wikipedia', 300), ('lexvec', 'crawl', 300),
+                                 ('word2vec', 'googlenews', 300), ('numberbatch', 'conceptnet', 300)])
